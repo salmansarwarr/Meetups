f4a159e730e11b38d5c4eadc0ea4bb42387ad255
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2023-01-05 17:48:54 +0500

    hide password

--- src/pages/api/get-edit-meetup.js
+++ src/pages/api/get-edit-meetup.js
@@ -7,7 +7,7 @@ async function handler(req, res) {
     if(req.method == "POST") {
         const id = req.body;
 
-        const client = await MongoClient.connect('mongodb+srv://msalmansarwar:1ka2ka3ka4@cluster0.0iczgaa.mongodb.net/meetups?retryWrites=true&w=majority')
+        const client = await MongoClient.connect('mongodb+srv://msalmansarwar:*password*@cluster0.0iczgaa.mongodb.net/meetups?retryWrites=true&w=majority')
         const db = client.db(); //selects database
 
         const meetupsCollections = db.collection('meetups');  //selects collection if exists, otherwise create collection
